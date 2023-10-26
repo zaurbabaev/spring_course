ae4a9f32b674298480c4bba2bcc6541d955cd255
Zaur <zaurik87@gmail.com>
2023-10-26 19:26:19 +0400

    first mvc_app

---
+++ spring_course_mvc/src/main/java/az/zaurbabayev/spring/mvc/MyController.java
@@ -0,0 +1,14 @@
+package az.zaurbabayev.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView(){
+        return "first-view";
+    }
+
+}
---
+++ spring_course_mvc/src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<h3>I wish you luck in Spring MVC learning</h3>
+</body>
+</html>
