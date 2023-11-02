1547ad3c2916db1f86f9441e8af1f2c4298c8bbf
Zaur <zaurik87@gmail.com>
2023-11-02 21:37:31 +0400

    employees/{id}

--- spring_course_rest/src/main/java/az/zaurbabayev/spring/rest/controller/MyRESTController.java
+++ spring_course_rest/src/main/java/az/zaurbabayev/spring/rest/controller/MyRESTController.java
@@ -5,6 +5,7 @@ import az.zaurbabayev.spring.rest.entity.Employee;
 import az.zaurbabayev.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class MyRESTController {
     public List<Employee> showAllEmployees() {
         List<Employee> allEmployees = employeeService.getAllEmployees();
         return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        return employee;
     }
 
 
