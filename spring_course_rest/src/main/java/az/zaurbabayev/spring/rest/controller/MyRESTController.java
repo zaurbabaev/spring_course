ec1d3f3f272ca7ddc663f64b18dbe562f92002b0
Zaur <zaurik87@gmail.com>
2023-11-03 12:57:55 +0400

    GlobalExceptionHandler
    GlobalExceptionHnadler üçün əlavə bir klass yaradılır və ExceptionHandlerlə bağlı metodlar bu klasa əlavə olunur. klasın üzərində @ControllerAdvice əlavə olunur

--- spring_course_rest/src/main/java/az/zaurbabayev/spring/rest/controller/MyRESTController.java
+++ spring_course_rest/src/main/java/az/zaurbabayev/spring/rest/controller/MyRESTController.java
@@ -35,17 +35,5 @@ public class MyRESTController {
         return employee;
     }
 
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
 
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
-    }
 }
