e3dcef5ce7ecf9d181a8dbc25048c25e38361e19
Zaur <zaurik87@gmail.com>
2023-11-04 20:25:06 +0400

    configuration Spring Security project

---
+++ spring_course_security/src/main/java/az/zaurbabayev/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,29 @@
+package az.zaurbabayev.spring.security.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("zaur")
+                        .password("zaur")
+                        .roles("EMPLOYEE"))
+                .withUser(userBuilder.username("elena")
+                        .password("elena")
+                        .roles("HR"))
+                .withUser(userBuilder.username("ivan")
+                        .password("ivan")
+                        .roles("MANAGER", "HR"));
+
+    }
+}
